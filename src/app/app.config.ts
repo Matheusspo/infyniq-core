15dc6f726c0c7668336514381a98c07bbb148776
Matheus Oliveira <matheus.oliveira@toroinvestimentos.com.br>
2026-02-08 18:17:43 -0300

    primeiro commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
+import Aura from '@primeuix/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideAnimations(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
+  ]
+};
